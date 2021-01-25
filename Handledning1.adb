45ac2c329af651e172af3d8b6809eedba5069ed8
cealestis <61761730+cealestis@users.noreply.github.com>
2021-01-25 21:32:11 +0100

    Update Handledning1.adb

--- Handledning1.adb
+++ Handledning1.adb
@@ -38,24 +38,23 @@ Y: Integer;
 S: String(1..6);
 
 begin
-Y:= 0; -- y har värdet noll om den ej innehåller c
+ Y:= 0; -- y har värdet noll om den ej innehåller c
 
-Put("Skriv in ett ord med 6 små bokstäver: ");
-Get(S);
+  Put("Skriv in ett ord med 6 små bokstäver: ");
+   Get(S);
 
-for I in 1..6 loop
+ for I in 1..6 loop
 
-if S(I) = 'c' then
-Y:=1;
-Put_Line("'c' finns!");
+   if S(I) = 'c' then
+   Y:=1;
+   Put_Line("'c' finns!");
 
-end if;
+  end if;
 end loop;
 
-if Y=0 then
-Put_Line("'c' finns inte");
-end if;
-
+  if Y=0 then
+   Put_Line("'c' finns inte");
+  end if;
 end Hand0;
 
 
