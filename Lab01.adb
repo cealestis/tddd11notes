5cf09ee9025b479243fdc2a937dea38935f5f332
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-01 16:24:15 +0100

    Update Lab01.adb

--- Lab01.adb
+++ Lab01.adb
@@ -1,6 +1,46 @@
 with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO;
 use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO;
 
+procedure Test1 is
+   
+   Star: Character := '*';
+   I,J: Integer := 0;
+   
+begin
+   
+   --vanlig loop
+   Loop
+     Put(Star);
+     I:= I + 1;
+     exit when I = 10;
+   end loop;
+   
+   New_Line;
+   --while
+   
+   while J < 10 loop
+      J:= J + 1;
+      Put(Star);
+   end loop;
+   
+   New_Line;
+   --for
+   
+   for A in 1..10 loop
+      Put(Star);
+   end loop;
+   
+   
+      
+end Test1;
+
+
+----------
+
+
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO;
+
 procedure Lab0 is
    
    F, D, K:Float;
@@ -35,6 +75,7 @@ begin
       New_Line;
       exit when Tot < 0.0;
       end loop;
+     
       
    
 end Lab0;
