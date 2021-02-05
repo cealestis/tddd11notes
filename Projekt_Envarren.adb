88829437ea0547812f4b42997bbb4aea4417bbab
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-05 18:42:19 +0100

    Create Projekt_Envarren.adb

---
+++ Projekt_Envarren.adb
@@ -0,0 +1,65 @@
+ with Ada.Integer_Text_IO, Ada.Text_IO; 
+use Ada.Integer_Text_IO, Ada.Text_IO; 
+
+procedure Envarre is
+   
+   ----------------------
+
+   procedure Vecka (V: out Integer) is
+      
+   begin
+      
+      loop
+	 Get(V);
+	 
+	 if V <= 3 or V > 52 then
+	    Put_Line("Måste vara mellan 3 och 52.");
+	    Put("Skriv in vilken vecka det är: ");
+	 end if;
+	 
+	 exit when V >= 3 and V < 52; 
+	 
+      end loop;      
+      
+   end Vecka;
+   
+   ----------------------
+   
+   function Antal_F (V: in Integer) return integer is  --räkna antalet fö/vecka     
+          FV: Integer := 0;  
+   begin
+      
+      if V = 3 then
+	 FV:= 2;
+      elsif V = 4 then  --specialfallen då vi ej räknar från 0
+	 FV:= V;
+      else
+	 for Counter in 1..V-4 loop   -- 1..V-4 då nollstället är på vecka 4     
+	    FV:= V + Counter; 	 
+	 end loop;      
+      end if;
+      
+      return FV;
+      
+   end Antal_F;
+
+   ---------------------- 
+   V:Integer;
+   
+begin
+   
+   Put("Skriv in vilken vecka det är: ");
+   Vecka(V);
+   
+   New_Line;
+   
+   Put("Du skrev in veckan: ");
+   Put(V,1);
+
+   New_Line;
+   
+   Put("Då bör du vara på föreläsning: ");
+   Put(Antal_F(V),1);   
+   
+   
+end Envarre;
