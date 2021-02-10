4616496ecce19aa32583840a8fa6fadc9e0350a1
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-10 12:44:40 +0100

    Update Figurer_Loop.adb

--- Figurer_Loop.adb
+++ Figurer_Loop.adb
@@ -1,35 +1,60 @@
-with Ada.Text_IO;         use Ada.Text_IO;
+---------------------
+with Ada.Text_IO;  use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 procedure Underp is      
    
-   ------------------ Inmat
-   procedure Inmat (A,B,C: out Integer) is
+   ------------------ 
+   procedure Inmat (P1,P2,P3: out Integer) is
    begin
-      loop      
-	 Put("Mata in pucklar på de tre ”dromedarerna”: ");
-	 Get(A); Get(B); Get(C);
-	 
-	 if A <= 0 or B <= 0 or C <= 0 then
-	    Put_Line("Måste vara positivt.");
-	 end if;      
-	 
-	 exit when A >= 1 and B >= 1 and C >= 1;
-      end loop;
-      
+      Put("Mata in pucklar på de tre ""dromedarerna"": "); 
+      Get(P1);
+      Get(P2);
+      Get(P3);
+
    end Inmat;
    
    ------------------
-   procedure En_Drom (in out Integral);
-   
+   procedure En_Dromedar (P: in integer) is
+   begin
+      
+      for C in 1..P loop --skriver ut en dromedar på varje 
+	 Put(" _ "); 
+      end loop;
+      
+      New_Line;
+      
+      for C in 1..P loop
+	 Put("/ \"); 
+      end loop;
+      
+      New_Line;
+   end En_Dromedar;   
    
    ------------------
-   A,B,C: Integer;
-begin
+   procedure Fler_Dromedarer (P1,P2,P3: in integer) is 
+      Sum: Integer := 0;
+   begin
+      
+      En_Dromedar(P1);       
+      En_Dromedar(P2);
+      En_Dromedar(P3);
+      
+      New_Line;
+      Sum:= P1 + P2 + P3;
+      Put_Line("Alla ""dromedarers"" pucklar på led: ");
+      En_Dromedar(Sum);
+      
+   end Fler_Dromedarer;   
+   
+   ------------------
 
-   Inmat(A,B,C);
+   P1,P2,P3: Integer;
    
-   
+Begin
+
+   Inmat(P1,P2,P3);
+   Fler_Dromedarer(P1,P2,P3);         
    
 end Underp;
 
