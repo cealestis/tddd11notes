e2dd64dbb7e9950e595294f93ad4de12fb3a7a76
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-12 16:43:58 +0100

    Update Lab03.adb

--- labbar/Lab03.adb
+++ labbar/Lab03.adb
@@ -1,3 +1,58 @@
+--------------------
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
+
+procedure Lab03 is
+   
+   ------
+   type Boolean_Arr_Type is     
+     array (1..5) of Boolean;
+   
+   ------
+   procedure Get_Convert(C: out Character; B: out Boolean) is
+   begin
+      
+      Get(C);
+      
+      if C = 'Y' then
+	 B:= True;
+      elsif C = 'N' then
+	 B:= False;
+      end if;      
+            
+   end Get_Convert;   
+   ------
+   
+   C:Character;
+   BA: Boolean_Arr_Type;
+begin      
+   
+   Put("Mata in 5 tecken: ");
+      
+   for Index in Boolean_Arr_Type'Range loop
+      Get_Convert(C, BA(Index));
+            
+      if Index = Boolean_Arr_Type'First then
+	 New_Line;
+	 Put_Line("Fältet innehåller: ");
+	 Put_Line("Index:Värde");
+      end if;      
+      
+      Put(Index,2);
+      Put(':');
+      
+      if BA(Index) = True then
+	 Put("Sant");
+      else
+	 Put("Falskt");
+      end if;      
+      
+      New_Line;      
+   end loop;      
+   
+end Lab03;
+
+
 ---------------------
 
 with Ada.Text_IO, Ada.Integer_Text_IO;
