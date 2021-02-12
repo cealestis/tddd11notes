2d8be55a722978a088c446a46fa6f2e41b46d8d2
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-12 13:24:25 +0100

    Update DOJO_D4.adb

--- DOJO_D4.adb
+++ DOJO_D4.adb
@@ -1,17 +1,21 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Reg_Plate_Handling; use Reg_Plate_Handling;
 
-R: Reg_Plate_Type; -- en typ för att lagra ett registrerings6 
--- nummer för en bil. Har Tre tecken och
--- ett (tresiffrigt) heltal.
+R: Reg_Plate_Type;  -- en typ för att lagra ett registrerings6 
+                    -- nummer för en bil. Har Tre tecken och
+                    -- ett (tresiffrigt) heltal.
 
 begin
-Put("Mata in en regplåt: ");
-Get(R); -- Inmatningsformat "ABC-XYZ" där A, B och C är tecken 
-  -- och XYZ är siffror.
-Put_Line("Inmatningen OK.");
+
+  Put("Mata in en regplåt: ");
+  Get(R); -- Inmatningsformat "ABC-XYZ" där A, B och C är tecken 
+    -- och XYZ är siffror.
+  Put_Line("Inmatningen OK.");
+  
 exception
-when Format_Error =>
-Put_Line("Ogiltigt format för regplåten!");
+  when Format_Error =>
+    Put_Line("Ogiltigt format för regplåten!");
 
 end Test_Reg_Format;
+
+--ovanstående får aldrig ändras
