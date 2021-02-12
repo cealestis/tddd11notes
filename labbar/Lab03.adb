d6a96662b68546470c8cc5839a0c800e275b5c07
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-12 14:52:15 +0100

    Update Lab03.adb

--- labbar/Lab03.adb
+++ labbar/Lab03.adb
@@ -1,3 +1,39 @@
+---------------------
+
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
+
+procedure Lab03 is
+   
+   type Heltal_Type is     
+     array (-2..7) of Integer;
+   
+   H: Heltal_Type;
+begin      
+   
+   Put("Mata in: ");
+   
+   
+   for I in Heltal_Type'Range loop
+      Get(H(I));
+            
+      if I = Heltal_Type'First then
+	 New_Line;
+	 Put_Line("I fältet finns nu: ");
+	 Put_Line("Index:Värde");
+      end if;
+      
+      Put(I,3); Put(':');
+      Put(H(I),3);
+      New_LIne;
+      
+   end loop;
+      
+end Lab03;
+
+
+--------------------------------
+
 with Ada.Text_IO, Ada.Integer_Text_IO;
 use Ada.Text_IO, Ada.Integer_Text_IO;   -- vanlig array som inmatas 10 heltal och puttar dem enligt if-sats i for-loop.
 
