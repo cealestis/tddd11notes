0fcd0149641881d4068d173a2fed22867a180ac9
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-19 12:49:08 +0100

    Update Lab03.2.adb

--- labbar/Lab03.2.adb
+++ labbar/Lab03.2.adb
@@ -1,3 +1,91 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+
+procedure Lab032 is         
+   -----------------------------------------------------------------------------
+   type Person_Attr_Type is
+      record
+	 Colour : String (1..10);
+	 Colour_Len: Integer;
+	 Length: Integer;
+      end record;
+   -----------------------------------------------------------------------------
+   type Person_Data_Type is
+      record
+	 Arms : Integer;
+	 Heads  : Integer;
+	 Is_4D : Boolean;
+	 Attr : Person_Attr_Type;
+      end record;    
+  -----------------------------------------------------------------------------
+ 
+  procedure Inmat(item : out Person_Data_Type) is
+     Is_4D_S: String(1..10);
+     Is_4D_Length: Integer;
+  begin
+    
+    Put_Line("Ange dina egenskaper");
+    Put("- Antal huvuden: ");
+    Get(Item.Heads);
+    Put("- Antal armar: ");
+    Get(Item.Arms);
+    Skip_Line;
+    Put("- Är din dimension i 4D? ");   
+    Get_Line(Is_4D_S, Is_4D_Length);
+    if Is_4D_Length = 10 then
+       Skip_Line;
+    end if;    
+    
+    if Is_4D_Length = 3 then
+       Item.Is_4D:= False;
+    elsif Is_4D_Length = 2 then
+       Item.Is_4D:= True;
+    end if;
+    
+    Put_Line("Ange hur din T-shirt skall vara: ");
+    Put("- Färg: ");
+    Get_Line(Item.Attr.Colour, Item.Attr.Colour_Len);
+    if Item.Attr.Colour_Len = 10 then
+       Skip_Line;
+    end if;    
+    
+    Put("- Längd: ");
+    Get(Item.Attr.Length);
+    
+  end Inmat;
+  
+  -----------------------------------------------------------------------------
+  
+  P: Person_Data_Type;
+begin
+   
+   Put_Line("Välkommen till Intergalactic T-shirt Store.");
+   Inmat(P);   
+   
+   New_Line;
+   Put("Du har ");
+   Put(P.Heads,0);
+   Put(" huvud(en), ");
+   Put(P.Arms,0);   
+   Put(" arm(ar).");
+   
+   if P.Is_4D then
+      Put(" Du är i dimension 4D.");
+   end if;
+   
+   New_Line;
+   
+   Put("Du har just beställt en '");
+   Put(P.Attr.Colour (1..P.Attr.Colour_Len));
+   Put("' T-shirt med längd ");
+   Put(P.Attr.Length,0);
+   Put(" cm.");
+     
+end Lab032;
+
+------------------------------------------------------
+
 with Ada.Text_IO;			use Ada.Text_IO;
 with Ada.Integer_Text_IO;		use Ada.Integer_Text_IO;
 
