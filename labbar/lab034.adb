03d69e0fb9fd1f4df2cd8b178863d1e4ef418acb
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-20 12:04:35 +0100

    Create lab034.adb
    Mirremirai bästakingen

---
+++ labbar/lab034.adb
@@ -0,0 +1,35 @@
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Command_Line;
+use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Command_Line;
+
+procedure Lab034 is
+   
+   I : Integer;
+   B : Boolean := False;
+   
+begin
+   
+   I := Command_Name'Length - 2;
+   
+   if Argument (1) = "=" then      
+      if I = Argument (2)'Length then
+	 B := True;
+      end if;            
+   elsif Argument (1) = "<" then      
+     if I < Argument (2)'Length then
+	 B := True;
+      end if;             
+   elsif Argument (1) = ">" then      
+      if I > Argument (2)'Length then
+	 B := True;
+      end if;            
+   end if;
+   
+   Put ("Det var");   
+   if B then 
+      null;
+   else
+      Put (" inte");
+   end if;   
+   Put (" ok!");      
+   
+end Lab034;
