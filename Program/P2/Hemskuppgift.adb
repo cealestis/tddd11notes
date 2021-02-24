6f19aab3858adbf6344d60ac3a31f5c8ea3a581e
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-24 09:27:19 +0100

    Update Hemskuppgift.adb

--- Program/P2/Hemskuppgift.adb
+++ Program/P2/Hemskuppgift.adb
@@ -1,96 +1,74 @@
-with Ada.Text_IO;  use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
-with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
+with Ada.Text_IO;			use Ada.Text_IO;
+with Ada.Float_Text_IO;			use Ada.Float_Text_IO;
+with Ada.Integer_Text_IO;		use Ada.Integer_Text_IO;
+with Ada.Numerics.Elementary_Functions;	use Ada.Numerics.Elementary_Functions;
 
-procedure P2 is      
-   ------------------------
-   procedure Inmat (X,Y,Z: out Float) is
-   begin
+procedure P2 is
+  
+  -----------------------------------------------------------------------------
+  procedure Get(X, Y, Z : out Integer) is
+    
+  begin
+    Get(X);
+    Get(Y);
+    Get(Z);
+  end Get;
       
-      Put("Mata in en koordinat (x, y, z): ");
-      Get(X);
-      Get(Y);
-      Get(Z);            
-      
-   end Inmat;   
-   ------------------------
-   function Kvadrera (I: in Float) return Float is
-      Mult: Float;
-   begin
-      
-      Mult:= I*I;
-      return Mult;
-      
-   end Kvadrera;   
-   ------------------------
-   function Distance (X,Y,Z: in Float) return Float is
-      Sum, Rot: Float;
-   begin
-      
-      Sum:= Kvadrera(X) + Kvadrera(Y) + Kvadrera(Z);
-      Rot:= Sqrt(Sum);
-      
-      return Rot;
-      
-   end Distance;   
-   ------------------------
-   
-   procedure Distancia (X,Y,Z: in Float; Roten: out Float) is
-      Sum: Float;
-   begin
-      
-      Sum:= Kvadrera(X) + Kvadrera(Y) + Kvadrera(Z);
-      Roten:= Sqrt(Sum);
-      
-   end Distancia;
-   ------------------------
-   
-   procedure Utmat (X,Y,Z: in Float) is
-      Rutis: Float;
-   begin
-      
-      Put("Detta ligger på avstånd ");
-      Put(Distance(X,Y,Z),0,2,0);
-      Put_Line(" från origo (enligt funktionen)."); 
-      
-      Put("Detta ligger på avstånd ");
-      Distancia(X,Y,Z,Rutis);
-      Put(Rutis,0,2,0);
-      Put_Line(" från origo (enligt proceduren)."); 
-            
-   end Utmat;   
-   ------------------------
-   procedure Koordinatfalt (X,Y,Z: in Float) is
-   begin 
-      
-      for Rader in 1..10 loop
-	 Put_Line(". . . . . . . . . . . . . . . . . . . . .");
+  -----------------------------------------------------------------------------
+  function Calc_Distance(X, Y, Z : in Integer) return Float is
+    
+  begin
+    return Sqrt(Float(X) * Float(X) + Float(Y) * Float(Y) + Float(Z) * Float(Z)); --kan stoppa i floats sådär
+  end Calc_Distance;
+  
+  ----------------------------------------------------------------------------
+  procedure Calc_Distance(X, Y, Z : in     Integer;
+			  Dist    :    out Float) is
+    
+  begin
+    Dist := Calc_Distance(X, Y, Z);
+  end Calc_Distance;
+  
+  -----------------------------------------------------------------------------
+  procedure Print(X, Y : in Integer) is
+    
+    Xx : Integer := Integer(Float(X)/10.0); --tbx till integer och delat på 10 för graderna på koordinaten
+    Yy : Integer := Integer(Float(Y)/10.0); 
+    
+  begin
+     for Yaxeln in -10 .. 10 loop  --dessa räknar ju antal prickar, och när de är på den countern 
+					   --som är samma som floaten delat på tio, ex. om float är 100 blir det 10, 
+					   --så kommer den sätta sig längst bort vid 10an
+	-- Y axeln går 10 till -10 i reverse då position 10 är högst upp, dvs positivt upp negativt ner
+	for Xaxeln in reverse -10 .. 10 loop   
+	if (Xaxeln = Xx) and (Yaxeln = Yy) then --X axeln går som vanligt, -10 till 10
+	  Put(" K");
+	elsif (Xaxeln = 0) and (Yaxeln = 0) then
+	  Put(" O");
+	else
+	  Put(" .");
+	end if;
       end loop;
-      
-      Put(". . . . . . . . . . ");
-      
-      if Distance(X,Y,Z) = 0.0 then	
-	 Put(" K ");
-      else
-	 Put(" o ");
-      end if;
-      
-      Put_Line(" . . . . . . . . . ");
-      
-      for Rader in 1..10 loop
-	 Put_Line(". . . . . . . . . . . . . . . . . . . . .");
-      end loop;
-   end Koordinatfalt;
-   ------------------------
-   
-   X,Y,Z: Float;
+      New_Line;
+    end loop;
+  end Print;
+
+  -----------------------------------------------------------------------------
+  X, Y, Z : Integer;
+  Dist    : Float;
+  
 begin
-   
-   Inmat(X,Y,Z);
-      
-   Utmat(X,Y,Z);   
-   
-   Koordinatfalt(X,Y,Z);
-   
+  Put("Mata in en koordinat (x, y, z): ");
+  Get(X, Y, Z);
+  
+  Calc_Distance(X, Y, Z, Dist);
+  Put("Detta ligger på avstånd ");
+  Put(Dist,0,2,0);
+  Put_Line(" från origo (enligt proceduren).");
+  
+  Put("Detta ligger på avstånd ");
+  Put(Calc_Distance(X, Y, Z),0, 2,0);
+  Put_Line(" från origo (enligt funktionen).");
+  
+  Print(X, Y);
 end P2;
