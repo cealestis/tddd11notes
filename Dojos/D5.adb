46070da4cbf0d481a5073b4eca8622321a8f45b3
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-26 15:29:18 +0100

    Update D5.adb

--- Dojos/D5.adb
+++ Dojos/D5.adb
@@ -1,32 +1,16 @@
-with Ada.Text_IO, Ada.Integer_Text_IO; 
+With Ada.Text_IO, Ada.Integer_Text_IO; 
 use Ada.Text_IO, Ada.Integer_Text_IO;
 
-procedure Lab05 is    
+procedure Lab05 is            
    
-   function One_Layer (Layer: in Integer) return Integer is
-   begin
+   function Stairs (Layer: in Integer) return Integer is --räknar stenar
+   begin                   
       
-      return Layer*(2*Layer-1);
-      
-   end One_Layer;
-   
-   
-   function Stairs (Item: in Integer) return Integer is --räknar stenar
-   begin
-           
-      --Första Lagret:= 1 = 1(2*1-1);
-     -- Andra:= 2 * 3 = 2(2*2 - 1);      
-      --kalla på funktionen igen för att plussa på varje lager innan
-            
-      
-      if Item = 0 then
+      if Layer = 0 then
 	 return 0;
       end if;
       
-      	 return One_Layer(Item) + Stairs(Item-1); 
-
-      --tänk på att man har den innan eller efter if-satsen
-      --Ex. höjd item = 2 så 2 * One_Layer(2) + Stairs(Item-1)
+      return Layer*(2*Layer-1) + Stairs(Layer-1); 
       
    end Stairs;   
         
