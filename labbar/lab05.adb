eb6911bcc4dd490233c0a5faadfded635e069c27
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-26 16:37:51 +0100

    Update lab05.adb

--- labbar/lab05.adb
+++ labbar/lab05.adb
@@ -1,3 +1,35 @@
+
+------------
+
+With Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO; 
+use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO;
+
+procedure Lab05 is
+   
+   function Power(X : in Float; N : in Integer) return Float is
+   begin
+      
+      if N = 1 then
+	 return X;
+      else
+	return X * Power(X,N-1);
+      end if;      
+      
+   end Power;
+   
+   X: Float; N: Integer;
+begin
+   
+   X:= 2.0;
+   N:= 3; --X^N = Power(2,2) = 2^2 = 2*2 = 2 * 2*1 = X * Power(2, 2-1) = Power(2,1)
+   
+   Put(Power(X,N),0,1,0);
+   
+end Lab05;
+
+
+
+----------
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
