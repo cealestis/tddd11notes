0d833811abf814071eb06ee5963d337c5abfa8f8
cealestis <61761730+cealestis@users.noreply.github.com>
2021-02-26 16:17:58 +0100

    Create Rekursiv_test_summatalföljd.adb

---
+++ Rekursiv_test_summatalföljd.adb
@@ -0,0 +1,29 @@
+With Ada.Text_IO, Ada.Integer_Text_IO; 
+use Ada.Text_IO, Ada.Integer_Text_IO;
+
+procedure Lab05 is
+   
+   function Sum (N: in Integer) return Integer is
+   begin      
+      if N = 0 then
+	 Put(" = ");
+	 return N;
+      elsif N > 1 then
+	 Put(N,2); Put(" +");	 
+	 return N + Sum(N-1); 
+      else 
+	 Put(N,2);
+	 return N + Sum(N-1);		
+      end if;            
+   end Sum;
+   
+   N: Integer;
+begin
+   
+   Put("Mata in en siffra: ");
+   Get(N);
+   
+   Put("Talföljdens summa:");
+   Put(Sum(N),0);
+   
+end Lab05;
